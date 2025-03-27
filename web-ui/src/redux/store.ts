69d133ab38677f069405b14cd283779322669a83
Shubhada-BrilliantechSoft <shubhada@brilliantechsoft.com>
2025-03-27 11:52:57 +0530

    new files

---
+++ web-ui/src/redux/store.ts
@@ -0,0 +1,20 @@
+import { configureStore } from "@reduxjs/toolkit";
+import registerDoctorReducer from './doctor/registerDoctorSlice';
+import signInDoctorReducer from './doctor/loginDoctorSlice'
+import { signInDoctor } from "./doctor/loginDoctorSlice";
+
+const store = configureStore({
+    reducer: {
+        registerDoctor: registerDoctorReducer,
+        signInDoctor : signInDoctorReducer,
+    },
+    middleware: (getDefaultMiddleware) => 
+        getDefaultMiddleware({
+            serializableCheck:false,
+        })
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
