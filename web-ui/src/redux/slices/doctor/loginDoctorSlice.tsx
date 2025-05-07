76baacd74cb7fc64dbbe425aea5af9536c0d9b3d
kartikbtspl <kartik@brilliantechsoft.com>
2025-05-07 12:59:49 +0530

    revert code

--- web-ui/src/redux/slices/doctor/loginDoctorSlice.tsx
+++ web-ui/src/redux/slices/doctor/loginDoctorSlice.tsx
@@ -4,7 +4,7 @@ import { fetchDoctorProfile } from "./doctorProfileSlice";
 import { API_BASE_URL } from "../../../config/apiConfig";
 
 const LOGIN_DOCTOR_URL =
-  "https://ad53-203-192-220-137.ngrok-free.app/api/auth/login";
+  import.meta.env.VITE_BACKEND_URL+"/api/auth/login";
 
 interface User {
   id: string;
